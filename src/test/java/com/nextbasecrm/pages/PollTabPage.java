83d37a75852f974f2c582830579fc0081fb34818
suatff <102978126+suatff@users.noreply.github.com>
2022-11-13 20:47:58 +0100

    I created feature file

--- src/test/java/com/nextbasecrm/pages/PollTabPage.java
+++ src/test/java/com/nextbasecrm/pages/PollTabPage.java
@@ -1,4 +1,24 @@
 package com.nextbasecrm.pages;
 
+import com.nextbasecrm.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class PollTabPage {
+
+    public PollTabPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+    @FindBy(xpath = "//a[.='Add more']")
+    public WebElement addMoreButton;
+
+    @FindBy(xpath = "//a[.='Employees and departments']")
+    public WebElement employeesAndDepartments;
+    @FindBy(xpath = "//a[@href='#DR647']")
+    public WebElement groupOfContacts;
+    @FindBy (xpath = "//a[@rel='DR647']")
+    public WebElement allDepartmentAndSubdepartmentEmployees;
+
 }
