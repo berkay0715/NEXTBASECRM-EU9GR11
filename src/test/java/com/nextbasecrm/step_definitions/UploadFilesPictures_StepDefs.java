c1f02c4be897e7f8dedfc29e2a0ca44dea218981
Hicret Gazioglu <hicretgazioglu@gmail.com>
2022-11-14 20:22:08 +0100

    some changes...

--- src/test/java/com/nextbasecrm/step_definitions/UploadFilesPictures_StepDefs.java
+++ src/test/java/com/nextbasecrm/step_definitions/UploadFilesPictures_StepDefs.java
@@ -7,7 +7,12 @@ import com.nextbasecrm.utilities.BrowserUtils;
 import com.nextbasecrm.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
 
 public class UploadFilesPictures_StepDefs {
 
@@ -34,18 +39,17 @@ public class UploadFilesPictures_StepDefs {
 
     }
 
-    @When("user clicks -Upload files and images- option and upload file")
-    public void user_clicks_upload_files_and_images_option_and_upload_file() {
+    @When("user uploads file")
+    public void user_uploads_file() {
 
-       // String path2="C:\\Users\\hicre\\OneDrive\\Masaüstü\\GIThub  sessions\\branch.png";
-       // String path3="C:\\Users\\hicre\\OneDrive\\Masaüstü\\GIThub  sessions\\GIThub.png";
-
-       // uploadFilesAndPicturesPage.uploadFilesANDimagesLink.click();
-
-
+        String path2="C:\\Users\\hicre\\OneDrive\\Masaüstü\\AUTOMATION\\Cucumber GITIGNORE file.docx";
+        String path3="C:\\Users\\hicre\\OneDrive\\Masaüstü\\GIThub  sessions\\Github_1_Slides_.pptx";
         String path1= "C:\\Users\\hicre\\OneDrive\\Masaüstü\\GIThub  sessions\\branch on GIT.png";
-        BrowserUtils.sleep(5);
+
+        BrowserUtils.sleep(2);
         uploadFilesAndPicturesPage.uploadFilesANDimagesLink.sendKeys(path1);
+        uploadFilesAndPicturesPage.uploadFilesANDimagesLink.sendKeys(path2);
+        uploadFilesAndPicturesPage.uploadFilesANDimagesLink.sendKeys(path3);
 
         BrowserUtils.sleep(3);
     }
@@ -65,8 +69,90 @@ public class UploadFilesPictures_StepDefs {
 
     @Then("verify the user sent the files successfully")
     public void verify_the_user_sent_the_files_successfully() {
-        System.out.println("done");
+        uploadFilesAndPicturesPage.sentMultipleDifferentFiles.isDisplayed();
     }
+
+
+    @When("user uploads picture")
+    public void user_uploads_picture() {
+        String path4="C:\\Users\\hicre\\OneDrive\\Masaüstü\\HTML Class\\sunnyDay.jpg";
+        uploadFilesAndPicturesPage.uploadFilesANDimagesLink.sendKeys(path4);
+        BrowserUtils.sleep(3);
+    }
+    @Then("verify the user sent the picture successfully")
+    public void verify_the_user_sent_the_picture_successfully() {
+        uploadFilesAndPicturesPage.sentImage.isDisplayed();
+
+    }
+
+
+    @When("user uploads file and images then insert them into the text")
+    public void user_uploads_file_and_images_then_insert_them_into_the_text() {
+        String path2="C:\\Users\\hicre\\OneDrive\\Masaüstü\\AUTOMATION\\Cucumber GITIGNORE file.docx";
+        String path4="C:\\Users\\hicre\\OneDrive\\Masaüstü\\avatar.png";
+        uploadFilesAndPicturesPage.uploadFilesANDimagesLink.sendKeys(path2);
+        BrowserUtils.sleep(2);
+        uploadFilesAndPicturesPage.uploadFilesANDimagesLink.sendKeys(path4);
+        BrowserUtils.sleep(2);
+
+        List<WebElement> allInsertTextButton=
+                Driver.getDriver().findElements(By.xpath("//span[@class='insert-btn-text']"));
+
+        for (WebElement each : allInsertTextButton) {
+            each.click();
+        }
+
+        BrowserUtils.sleep(2);
+    }
+    @Then("verify the user inserted the files into the text successfully")
+    public void verify_the_user_inserted_the_files_into_the_text_successfully() {
+        WebElement insertedFile=
+                Driver.getDriver().findElement(By.xpath("//img[contains(@id,'disk-inline-image-')]"));
+        WebElement insertedFile2=
+                Driver.getDriver().findElement(By.xpath("//a[@class='feed-com-file-name']"));
+
+        insertedFile.isDisplayed();
+        insertedFile2.isDisplayed();
+    }
+
+
+    @When("user allows a recipient to edit document")
+    public void user_allows_a_recipient_to_edit_document() {
+        uploadFilesAndPicturesPage.allowRecipientToEditCheckbox.isEnabled();
+        System.out.println("' Allow a recipient to edit' checkbox is checked by default");
+    }
+    @Then("verify the user allowed a recipient to edit file successfully")
+    public void verify_the_user_allowed_a_recipient_to_edit_file_successfully() {
+        uploadFilesAndPicturesPage.editButton.isDisplayed();
+    }
+
+
+
+    @When("user remove the file")
+    public void user_remove_the_file() {
+        uploadFilesAndPicturesPage.delButton.click();
+    }
+    @Then("verify the file is removed successfully")
+    public void verify_the_file_is_removed_successfully() {
+       BrowserUtils.verifyElementNotDisplayed(By.xpath("//input[@value='sunnyDay']"));
+
+    }
+
+    @When("user rename the file before sending it")
+    public void user_rename_the_file_before_sending_it() {
+        uploadFilesAndPicturesPage.renameButton.isEnabled();
+
+        uploadFilesAndPicturesPage.renameButton.sendKeys(Keys.COMMAND+"a",Keys.BACK_SPACE);
+       // BrowserUtils.sleep(3);
+      //  uploadFilesAndPicturesPage.changeableFileName.sendKeys("new name"+ Keys.ENTER);
+      //  BrowserUtils.sleep(2);
+
+    }
+    @Then("verify the file is renamed successfully")
+    public void verify_the_file_is_renamed_successfully() {
+
+    }
+
 
 
 
--- src/test/java/com/nextbasecrm/utilities/Driver.java
+++ src/test/java/com/nextbasecrm/utilities/Driver.java
@@ -151,4 +151,6 @@ public class Driver {
             //driverPool.set(null);
         }
     }
+
+
 }
