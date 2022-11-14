6dc5f10570b3f90191258b7b1167ae411ed51fac
Berkay <berkay.s.celik@gmail.com>
2022-11-14 20:55:23 +0100

    change

--- src/test/java/com/nextbasecrm/runners/CukesRunner.java
+++ src/test/java/com/nextbasecrm/runners/CukesRunner.java
@@ -16,8 +16,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/nextbasecrm/step_definitions",
-        dryRun = true,
-        tags = "@Event"
+        dryRun = false,
+        tags = ""
        // publish = true
 )
 public class CukesRunner {
