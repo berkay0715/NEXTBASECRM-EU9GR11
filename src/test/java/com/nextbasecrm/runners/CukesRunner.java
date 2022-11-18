f21c7b051a2536671f532c82c3a5fcce40e2784e
Vedat DAGLAR <104867600+VedatDaglar@users.noreply.github.com>
2022-11-19 00:01:20 +0100

    Update CukesRunner.java

--- src/test/java/com/nextbasecrm/runners/CukesRunner.java
+++ src/test/java/com/nextbasecrm/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/nextbasecrm/step_definitions",
         dryRun = false,
-        tags = "@errormessage",
+        tags = "",
         publish = true
         
 )
