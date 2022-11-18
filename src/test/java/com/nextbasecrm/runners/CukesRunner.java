a48e2f64ab5cc9f35f442f4d9c431cf5ca2a8a10
VedatDaglar <vedatdaglar@protonmail.com>
2022-11-18 17:20:04 +0100

    jenkins test

--- src/test/java/com/nextbasecrm/runners/CukesRunner.java
+++ src/test/java/com/nextbasecrm/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/nextbasecrm/step_definitions",
         dryRun = false,
-        tags = "@NEXTBASE-1531",
+        tags = "",
         publish = true
         
 )
